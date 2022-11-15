6349f4f6c38efece5917640f9052b0c8fb800090
Maia Bell <maiarose04@gmail.com>
2022-11-15 11:23:20 -0800

    Finds the text that includes green and changes to green

--- script/script.js
+++ script/script.js
@@ -31,5 +31,49 @@ function populateList(list){
     }
 }
 let shoppingList = ["Cheese", "Ham", "Bread", "Green Pepper", "Milk", "Coffee"];
-
 populateList(shoppingList);
+
+function changeListStyle(){
+    unorderedListElement.classList.add("squareList");
+    unorderedListElement.classList.remove("circleList");
+}
+changeListStyle()
+
+function updateImage(){
+    const imageElement = document.querySelector("#shoppingCart");
+    console.log(imageElement);
+    imageElement.src = "https://cdn-icons-png.flaticon.com/512/263/263142.png";
+    imageElement.height = 100;
+    imageElement.width = 100;
+    imageElement.alt = "shopping cart item"
+}
+updateImage()
+
+function findGreen(){
+    const listItems = document.querySelectorAll(".shopping li");
+    for (let i = 0; i < listItems.length; i++){
+        console.log(listItems[i])
+        if(listItems[i].textContent.includes("Green")){
+            listItems[i].classList.add("greenText")
+        }
+    }
+}
+findGreen();
+
+
+// const unorderedList = document.querySelector(".shopping")
+
+// function addToList(list){
+//     let l = prompt();
+//     for (let l = 0; l < list.length; l++){
+//         console.log(list[l]);
+//         const listItemElement = document.createElement("li");
+//         listItemElement.textContent = list[l];
+//         unorderedList.append(listItemElement);}
+//     if (l != "Done")
+//     unorderedList.append(l);
+//     else
+//     console.log("Have fun shopping!");}
+
+// let shoppingList2 = [];
+// addToList(shoppinglist2);
