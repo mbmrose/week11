dee3a6b1fbbeb972c49cc0745fb2e050e58bf402
Maia Bell <maiarose04@gmail.com>
2022-11-15 09:45:16 -0800

    Function practice in script

---
+++ script/script.js
@@ -0,0 +1,20 @@
+// function greet(name){
+//     console.log("Hello " + name + "!")
+// }
+// greet(prompt("What is your name?: "))
+
+
+// function greet (username){
+//     console.log(username)
+// }
+// greet("Neda")
+
+// let greet = function(name){
+//     console.log(`hello ${name}`);
+// }
+// greet(prompt("Enter a name: "));
+
+let greet = (name) => {
+    console.log(`Hello ${name}!`)
+}
+greet(prompt("Enter a name: "))
